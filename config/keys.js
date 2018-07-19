3699036e98dfc06d74a859101603d84fe9b46112
sandeep0814 <sandeep0814@gmail.com>
2018-07-19 08:13:39 +0530

    mongoose connection

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds243041.mlab.com:43041/mydevconnector`
+}
--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
 dotenv.load();
-app.get('/',(req,res) => {
-res.send('hello world')
+
+//db configuration
+const db = require('./config/keys').mongoURI;
+
+//connect to database
+mongoose
+    .connect(db)
+    .then(() => {
+        console.log('connected');
+    })
+    .catch(err => console.log(err));
+
+
+app.get('/', (req, res) => {
+    res.send('hello world')
 });
 
 const port = process.env.PORT || 5000;
 
-app.listen(port,()=> {
+app.listen(port, () => {
     console.log(`server running on port ${port}`);
 });
