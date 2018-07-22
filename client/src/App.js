5ed1bf0a976f2a472f53c776963026c238d02421
sandeep0814 <sandeep0814@gmail.com>
2018-07-22 11:27:28 +0530

    home page setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Landing from './components/layout/Landing';
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>My react app</h1>
+      <Landing />
       </div>
     );
   }
