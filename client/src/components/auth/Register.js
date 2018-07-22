6284e95a2ffb516b7dd295a4dbc1988e370b8d39
sandeep0814 <sandeep0814@gmail.com>
2018-07-22 21:59:37 +0530

    registration

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import axios from 'axios';
 class Register extends Component {
   constructor() {
     super();
@@ -24,7 +24,9 @@ class Register extends Component {
       password:this.state.password,
       password2:this.state.password2,
     }
-    console.log(newUser);
+    axios.post('/api/users/register',newUser)
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err.response.data))
   }
   render() {
     return (
