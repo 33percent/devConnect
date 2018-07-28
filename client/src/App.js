a5c47e91b742f35a2463ecc26e77e72087c6dff3
sandeep0814 <sandeep0814@gmail.com>
2018-07-28 09:17:58 +0530

    login page and nav bar setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
-import './App.css';
-import { Provider } from 'react-redux';
-import store from './store';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Landing from './components/layout/Landing';
 
-//check for token
+import './App.css';
+
+// Check for token
 if (localStorage.jwtToken) {
-  // set auth token in headers
+  // Set auth token header auth
   setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
   const decoded = jwt_decode(localStorage.jwtToken);
-  //set user and is authenticated
+  // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // TODO: Clear current Profile
+
+    // Redirect to login
+    window.location.href = '/login';
+  }
 }
 
 class App extends Component {
@@ -28,10 +43,10 @@ class App extends Component {
         <Router>
           <div className="App">
             <Navbar />
-            <Route path="/" component={Landing} exact />
+            <Route exact path="/" component={Landing} />
             <div className="container">
-              <Route path="/register" component={Register} exact />
-              <Route path="/login" component={Login} exact />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
             </div>
             <Footer />
           </div>
