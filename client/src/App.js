5bd8c901d1cecf8c1bcc4b755a5d69ba66a57d32
sandeep0814 <sandeep0814@gmail.com>
2018-07-28 21:52:05 +0530

    prottecting roytes

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route,Switch } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
-
 import { Provider } from 'react-redux';
 import store from './store';
-
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard'; 
-
+import PrivateRoute from './components/common/PrivateRoute';
 import './App.css';
 import { clearCurrentProfile } from './actions/profileAction';
 
@@ -49,7 +47,9 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
-              <Route exact path="/dashboard" component={Dashboard}/>
+              <Switch>
+              <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+              </Switch>
             </div>
             <Footer />
           </div>
--- client/src/components/common/Spinner.js
+++ client/src/components/common/Spinner.js
@@ -4,10 +4,10 @@ import spinner from './spinner.gif';
 export default () => {
   return (
     <div>
-      <img 
-      src={spinner} 
-      alt="Loading"
-      style={{width:'200px',margin:'auto',display:'block'}}
+      <img
+        src={spinner}
+        alt="Loading"
+        style={{ width: '200px', margin: 'auto', display: 'block' }}
       />
     </div>
   )
